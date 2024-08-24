ffb9403120be29c75887ed25b8062bbf25780e4a
rpparmar <106550380+rpparmar@users.noreply.github.com>
2024-08-24 20:23:34 +0530

    First commit of Domian 2 Project

---
+++ InsuranceSys.Domain2/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceSys.Domain
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ InsuranceSys.Domain2/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace InsuranceSys.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
