60636b4dd3bbd3044aa40a15f9001395ede19f6e
rpparmar <106550380+rpparmar@users.noreply.github.com>
2025-07-27 19:29:24 +0530

    layout aside design changes , created partial view for form submit buttons panel

---
+++ InsureSys.WebApp/Models/Common/SubmitButtonViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Insurancesys.web.Models.Common
+{
+    public class SubmitButtonViewModel
+    {
+        public bool IsEditMode { get; set; }
+        public string FormId { get; set; } =string.Empty;
+    }
+}
