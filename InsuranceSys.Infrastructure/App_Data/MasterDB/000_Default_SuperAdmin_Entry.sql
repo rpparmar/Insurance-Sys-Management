e4e7413ad2e3e279aa8e646ae59662659777b3c4
rpparmar <106550380+rpparmar@users.noreply.github.com>
2026-04-29 19:24:07 +0530

    Commit sample Super Admin SQL entry

---
+++ InsuranceSys.Infrastructure/App_Data/MasterDB/000_Default_SuperAdmin_Entry.sql
@@ -0,0 +1,4 @@
+INSERT INTO AgencyUsers
+(AgencyId,Username,PasswordHash,PasswordSalt,Email,DisplayName,[Role],IsActive,LastLoginAtUtc,CreatedAtUtc,UpdatedAtUtc,IsDeleted,FirstName,MiddleName,LastName,IsSubUser)
+VALUES
+(NULL,N'superadmin',N'buiYXAMgCqrv8y7obi0xI5rXreWcmz2zKR6K9jGF3e0=',N'0wcvLRqO5JNbXCYyDRCzDA==',N'admin@insuresys.local',N'System Administrator',1,1,NULL,'2026-04-29 12:39:01.5583465','2026-04-04 11:37:56.5845731',0,'Super',NULL,'Admin',0)
