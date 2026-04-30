cec66f3f93e812959e0cc89c38dacc82a8c85cd6
rpparmar <106550380+rpparmar@users.noreply.github.com>
2026-04-30 18:36:02 +0530

    Restrict Agent from getting access to customers

--- InsureSys.WebApp/Controllers/CustomerController.cs
+++ InsureSys.WebApp/Controllers/CustomerController.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Insurancesys.web.Controllers
 {
-    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "SuperAdmin,AgencyAdmin,Agent")]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "SuperAdmin,AgencyAdmin")]
     public class CustomerController(IMapper mapper, ICustomerService customerService, IDropDownBinderService dropDownBinderService, IInsuranceTypeService insuranceTypeService) : Controller
     {
         private readonly IMapper _mapper = mapper;
